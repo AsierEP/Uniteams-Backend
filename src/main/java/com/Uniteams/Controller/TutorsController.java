5f46dec66c27e91e33ab017825a1f0a30889829a
luisma-2601 <142758332+luisma-2601@users.noreply.github.com>
2025-10-22 18:02:49 -0400

    Tutors

--- src/main/java/com/Uniteams/Controller/TutorsController.java
+++ src/main/java/com/Uniteams/Controller/TutorsController.java
@@ -1,11 +1,18 @@
 package com.Uniteams.Controller;
 
-import com.Uniteams.Service.TutorsService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 import java.util.Map;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.Uniteams.Service.TutorsService;
 
 @RestController
 @RequestMapping("/api/tutors")
--- src/main/java/com/Uniteams/Service/TutorsService.java
+++ src/main/java/com/Uniteams/Service/TutorsService.java
@@ -1,8 +1,11 @@
 package com.Uniteams.Service;
 
-import org.springframework.stereotype.Service;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-import java.util.*;
+import org.springframework.stereotype.Service;
 
 @Service
 public class TutorsService {
